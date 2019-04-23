34f7e32f846c8b072b20cfb7c79bc32973c9538e
Matt <Matt@CMB.local>
2019-04-23 12:52:48 -0400

    Add feature test check that cart link increments to 1 from zero when product added

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |m|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(1),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They click on the details button" do
+    #SELECT PRODUCT
+    product = @category.products.all
+
+    # ACT
+    visit "/"
+    cart_badge = page.find('.navbar-right li:nth-child(4)')
+
+    # DEBUG / VERIFY CART INCREMENT UPON CLICKING ADD
+    expect(cart_badge.text).to include('0')
+    page.find('article:first-child .btn-primary').click
+    expect(cart_badge.text).to include('1')
+  end
+
 end
